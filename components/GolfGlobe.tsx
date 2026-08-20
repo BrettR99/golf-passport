15eade5e835d1337f3aff91e74046b43d355c787
BrettR99 <brett.rear@gmail.com>
2026-08-20 14:42:48 -0700

    Update GolfGlobe.tsx

--- components/GolfGlobe.tsx
+++ components/GolfGlobe.tsx
@@ -73,7 +73,7 @@ function makeFlag(course: Course, onClick: () => void) {
   return el;
 }
 
-export default function GolfGlobe(_props: { courses?: Course[] }) {
+export default function GolfGlobe({ courses = [] }: { courses?: Course[] }) {
   const globeRef = useRef<any>(null);
 
   const [courses, setCourses] = useState<Course[]>([]);
